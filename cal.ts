bc10c4f6b2044278709b1085036533a0b4d931e9
Nitoo kumari <160069703+nitoo124@users.noreply.github.com>
2024-03-13 16:11:29 -0700

    Add files via upload

---
+++ cal.ts
@@ -0,0 +1,65 @@
+import inquirer from "inquirer";
+
+let input = await inquirer.prompt([{
+    name : 'num1',
+    type :  'number',
+    message : "plz entre your first number :"
+
+},
+{
+    name : 'num2',
+    type :  'number',
+    message : "plz entre your 2nd number :"
+
+},
+
+{
+    name : 'operator',
+    type :  'list',
+    message : "plz select operation :",
+    choices : ["+", "-", "*", "/", "%", "**"]
+
+}
+])
+console.log(input);
+
+let {num1, num2, operator} = input;
+
+if ( num1 && num2 && operator) {
+    if (operator === "+") {
+        console.log(num1 + num2)
+        
+    }
+
+    if (operator === "-") {
+        console.log(num1 - num2)
+        
+    }
+
+    if (operator === "*") {
+        console.log(num1 * num2)
+        
+    }
+
+    if (operator === "/") {
+        console.log(num1 / num2)
+        
+    }
+
+    if (operator === "%") {
+        console.log(num1 % num2)
+        
+    }
+
+    if (operator === "**") {
+        console.log(num1 ** num2)
+        
+    }
+
+    
+} else {
+
+    console.log("plz entre valid operator");
+    
+}
+
